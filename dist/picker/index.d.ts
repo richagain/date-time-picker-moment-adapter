17c1cd229ffb9b8017d2d04e3d81806e05370161
Richard Zheng <richard.z@navig8group.com>
2024-12-02 15:02:48 +0800

    exclude dist folder

--- dist/picker/index.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="@richagain/angular-datetime-picker-moment-adapter" />
-export * from './public_api';
--- dist/picker/lib/moment-adapter/moment-date-time-format.class.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * moment-date-time-format.class
- */
-import { OwlDateTimeFormats } from '@danielmoncada/angular-datetime-picker';
-export declare const OWL_MOMENT_DATE_TIME_FORMATS: OwlDateTimeFormats;
